1b554239699add8d60f301be3d2367ddd0398c57
Brain Aboze <abozejohn@gmail.com>
2022-01-17 03:27:24 +0100

    Fixed backdrop bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Mobile navigation toggle button
 let nav_interative = () => {
     let burger = document.getElementById('burger');
-    let nav = document.getElementById('nav-bar');
-    let menu = document.getElementById('menu')
+    let navBar = document.querySelector('#nav-bar');
+    let nav = document.getElementById('menu')
     let menuLinks = document.querySelectorAll('#menu li');
     // Select HTML body 
     let body = document.body;
@@ -11,12 +11,17 @@ let nav_interative = () => {
     burger.addEventListener('click', ()=>{
         burger.classList.toggle('toggle');
 
-        menu.classList.toggle('active');
+        nav.classList.toggle('active');
 
         // Fixed the body section 
         body.classList.toggle('body-fixed');
-    })
 
+        // Remove the blur on the navigation bar (Mobile)
+        navBar.classList.toggle('remove-blur');
+
+        // Add the blur on the navigation bar (Mobile)
+        nav.classList.toggle('add-blur');
+    })
 };
 
 nav_interative();
