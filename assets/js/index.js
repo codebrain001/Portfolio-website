b72d00333c557bd7526ccb3174a13a74f7c9a1c3
Brain Aboze <abozejohn@gmail.com>
2022-01-27 11:15:00 +0100

    First iteration of navigation bar (mobile)

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if (navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show_menu');
+    })
+}
+
+if (navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show_menu');
+    })
+}
+
+// Remove menu mobile
+const navLink = document.querySelectorAll('.nav_link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show_menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
