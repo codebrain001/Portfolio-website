97af53be0c383adb91f0d75038b23580db944028
Brain Aboze <abozejohn@gmail.com>
2022-02-05 22:18:27 +0100

    Mobile view (skills  page)

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,27 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+// Skillset 
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills (){
+    let itemClass = this.parentNode.className;
+
+    for (i=0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close';
+    }
+    if (itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach(
+    (el)=>{
+        el.addEventListener('click', toggleSkills)
+    }
+)
+
+console.log(skillsContent)
