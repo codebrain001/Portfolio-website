8da7ee810925cdbbc4fb26ff3c9af002edc25e32
Brain Aboze <abozejohn@gmail.com>
2022-02-06 02:49:57 +0100

    Mobile view (qualification  page: First iteration)

--- assets/js/index.js
+++ assets/js/index.js
@@ -46,4 +46,24 @@ skillsHeader.forEach(
     }
 )
 
-console.log(skillsContent)
+
+// Qualification tab
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tab.forEach (tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+
+    })
+})
